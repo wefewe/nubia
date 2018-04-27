ebf5caf323074119ad6c3834f6a4626b82e7f537
FH0 <32861476+FH0@users.noreply.github.com>
2018-04-27 22:45:14 +0800

    Add files via upload

--- Backstage.sh
+++ Backstage.sh
@@ -1,22 +1,54 @@
 #!/bin/bash
-yum install unzip wget net-tools tar curl -y || apt-get install unzip wget net-tools tar curl -y
-
-#下载解压离线包
-area=$(curl -s http://ip-api.com/line|sed -n '3p')
-if [ "$area" = "CN" ];then
-    wget -N --no-check-certificate https://gitee.com/just1601/tiny-sh/blob/master/ssr_jzdh.zip
-else
-    wget -N --no-check-certificate https://raw.githubusercontent.com/FH0/nubia/master/ssr_jzdh.zip
-fi
-unzip -o ssr_jzdh.zip -d /usr/local/bin
-chmod -R 0777 /usr/local/bin/SSR-Bash-Python
-
-#生成管理后台
-echo -e '#!/bin/bash\n\nclear\n\nwp=/usr/local/bin/SSR-Bash-Python\n\necho \necho " 1.安装SSR"\necho " 2.安装V2Ray"\necho " 3.安装BBR"\necho " 4.安装锐速"\necho \necho " 5.进入SSR管理面板(ssr)"\necho " 6.进入V2Ray管理面板(v2)"\necho \necho " 7.卸载SSR"\necho " 8.卸载V2Ray"\necho \nread -p "请选择: " choice\n\nif [ "$choice" = 1 ];then\n    bash ${wp}/install.sh\nelif [ "$choice" = 2 ];then\n    bash ${wp}/jzdh/v2ray.sh\nelif [ "$choice" = 3 ];then\n    bash ${wp}/jzdh/bbr.sh\nelif [ "$choice" = 4 ];then\n    bash ${wp}/jzdh/serverspeeder.sh\nelif [ "$choice" = 5 ];then\n    ssr\nelif [ "$choice" = 6 ];then\n    v2\nelif [ "$choice" = 7 ];then\n    bash ${wp}/uninstall.sh\nelif [ "$choice" = 8 ];then\n    echo\n    read -p "回车继续"\n    systemctl stop v2ray\n    systemctl disable v2ray\n    rm -rf /usr/local/bin/v2ray > /dev/null 2>&1\n    rm -f /etc/systemd/system/v2ray.service > /dev/null 2>&1\n    rm -f /bin/v2\n    clear\n    echo " v2ray已停止并且已卸载"\n    echo\nelse\n    clear\n    echo "请重新输入"\n    echo\nfi\n' > /bin/jzdh
-chmod +x /bin/jzdh
-
-#完成后
-(sleep 5 && rm -r $0) > /dev/null 2>&1 &
 clear
-echo " 安装完成，输入jzdh进入后台"
+echo " 正在安装必要组件,请耐心等待"
+{
+    yum install unzip wget net-tools curl -y
+    apt-get install unzip wget net-tools curl -y
+} > /dev/null 2>&1 
+
+#用户选择
+clear
+echo "欢迎使用V2Ray&SSR搭建脚本"
+echo 
+echo " 1.安装SSR"
+echo " 2.安装V2Ray"
 echo
+echo " 3.安装BBR(安装完成后自动重启系统)"
+echo " 4.安装锐速(安装完成后自动重启系统)"
+echo
+read -p "请选择: " choice
+
+#操作
+if [ "$choice" = "1" ];then
+    clear
+    echo " 1.码云(国内平台)"
+    echo  " 2.github(国外平台)"
+    echo 
+    read -p "请选择资源包托管平台: " zip
+    if [ "$zip" = "1" ];then
+        wget -N --no-check-certificate https://gitee.com/just1601/tiny-sh/raw/master/ssr.zip
+    else
+        wget -N --no-check-certificate https://raw.githubusercontent.com/FH0/nubia/master/ssr.zip
+    fi
+    unzip -o ssr.zip
+    bash SSR-Bash-Python/install.sh
+elif [ "$choice" = "2" ];then
+    clear
+    echo " 1.码云(国内平台)"
+    echo  " 2.github(国外平台)"
+    echo 
+    read -p "请选择资源包托管平台: " zip
+    if [ "$zip" = "1" ];then
+        wget -N --no-check-certificate https://gitee.com/just1601/tiny-sh/raw/master/V2Ray.zip
+    else
+        wget -N --no-check-certificate https://raw.githubusercontent.com/FH0/nubia/master/V2Ray.zip
+    fi
+    unzip -o V2Ray.zip
+    bash V2Ray/v2ray.sh
+elif [ "$choice" = "3" ];then
+    bash <(curl -L -s https://raw.githubusercontent.com/FH0/nubia/master/bbr.sh)
+    reboot
+elif [ "$choice" = "4" ];then
+    bash <(curl -L -s https://raw.githubusercontent.com/FH0/nubia/master/serverspeeder.sh)
+    reboot
+fi
