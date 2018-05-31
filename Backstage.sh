6dfe26d3559974266bf80f6993a888ca5ea5d76b
FH0 <32861476+FH0@users.noreply.github.com>
2018-06-01 07:52:02 +0800

    Add files via upload

--- Backstage.sh
+++ Backstage.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+function centos_install {
+    yum update -y
+    rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+    rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+    yum --enablerepo=elrepo-kernel install kernel-ml -y
+    grub2-set-default 0
+    sed -i '/JZDH_bbr/d' /etc/rc.d/rc.local
+    echo '/bin/JZDH_bbr' >> /etc/rc.d/rc.local
+    chmod 0777 /etc/rc.d/rc.local
+    rm -f /bin/JZDH_bbr
+    echo -e 'sed -i '/net\.core\.default_qdisc=fq/d' /etc/sysctl.conf\nsed -i '/net\.ipv4\.tcp_congestion_control=bbr/d' /etc/sysctl.conf\necho "net.core.default_qdisc=fq" >> /etc/sysctl.conf\necho "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf\nsysctl -p\nsed -i '/JZDH_bbr/d' /etc/rc.d/rc.local\nrm -f \$0' > /bin/JZDH_bbr
+    chmod +x /bin/JZDH_bbr
+    reboot
+}
+
+function debian_bbr_start {
+    sed -i '/net\.core\.default_qdisc=fq/d' /etc/sysctl.conf
+    sed -i '/net\.ipv4\.tcp_congestion_control=bbr/d' /etc/sysctl.conf
+    echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+    echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+    sysctl -p
+}
+
+function install_bbr {
+    if [ "`cat /etc/*release | grep "CentOS"`" = "" ];then
+        debian_bbr_start
+    else
+        centos_install
+    fi
+}
+
 function person_setting {
     clear && echo 
     echo -e " 这是危险的操作，如果你不知道你在做什么，请立即停止它。"
@@ -46,16 +77,6 @@ function install_v2 {
     v2
 }
 
-function install_bbr {
-    bash <(curl -sL https://raw.githubusercontent.com/FH0/nubia/master/bbr.sh)
-    reboot
-}
-
-function install_serverspeeder {
-    bash <(curl -sL https://raw.githubusercontent.com/FH0/nubia/master/serverspeeder.sh)
-    reboot
-}
-
 function install_nginx {
     apt-get install nginx -y > /dev/null 2>&1
     rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm > /dev/null 2>&1
@@ -79,15 +100,13 @@ function pannel {
     [ -d "/usr/local/SSR-Bash-Python" ] && echo -e " 1.重装\033[32mSSR\033[0m"
     [ ! -d "/usr/local/SSR-Bash-Python" ] && echo -e " 1.安装SSR(输入ssr进入管理面板)"
     [ -d "/bin/v2ray" ] && echo -e " 2.重装\033[32mV2Ray\033[0m"
-    [ !-d "/bin/v2ray" ] && echo -e " 2.安装V2Ray(输入v2进入管理面板)"
+    [ ! -d "/bin/v2ray" ] && echo -e " 2.安装V2Ray(输入v2进入管理面板)"
     echo 
-    [ "`lsmod | grep -q bbr`" = "" ] && echo -e " 3.安装BBR(安装完成后自动重启系统)"
-    [ "`lsmod | grep -q bbr`" != "" ] && echo -e " 3.重装\033[32mBBR\033[0m"
-    [ "`ps -ef | grep "peeder" | grep -v "grep"`" = "" ] && echo -e " 4.安装锐速(安装完成后自动重启系统)"
-    [ "`ps -ef | grep "peeder" | grep -v "grep"`" != "" ] && echo -e " 4.重装\033[32m锐速\033[0m"
+    [ "`lsmod | grep bbr`" = "" ] && echo -e " 3.安装BBR(安装完成后自动重启系统)"
+    [ "`lsmod | grep bbr`" != "" ] && echo -e " 3.重装\033[32mBBR\033[0m"
     echo 
-    [ "`type nginx | grep "is"`" = "" ] && echo " 5.安装nginx(没有任何说明，慎装)"
-    [ "`type nginx | grep "is"`" != "" ] && echo -e " 5.更新\033[32mnginx\033[0m"    
+    [ "`which nginx`" = "" ] && echo " 4.安装nginx(没有任何说明，慎装)"
+    [ "`which nginx`" != "" ] && echo -e " 4.更新\033[32mnginx\033[0m"    
     echo && read -p "请选择: " choice
 
     #操作
@@ -95,8 +114,7 @@ function pannel {
     [ "$choice" = "1" ] && install_ssr
     [ "$choice" = "2" ] && install_v2
     [ "$choice" = "3" ] && install_bbr
-    [ "$choice" = "4" ] && install_serverspeeder
-    [ "$choice" = "5" ] && install_nginx
+    [ "$choice" = "4" ] && install_nginx
 }
 
 pannel
