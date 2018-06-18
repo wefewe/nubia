1dadbde2f6851fe71e0cd3a51bd02ddc7a96c2e6
FH0 <32861476+FH0@users.noreply.github.com>
2018-06-18 13:31:54 +0800

    Add files via upload

--- person_settings.sh
+++ person_settings.sh
@@ -3,12 +3,24 @@
 ssh_key() {
     rm -rf /root/.ssh
     mkdir /root/.ssh
-    echo -e 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDuwLr5N5CxF51tEOXtJJ3Qr2+uY7lVtZfWNwN59yewWUhc6p77CiWj917TrOgrgGMIIgb7AXU0vrdNr2IFJ0fNdyF9S9dfEU8+KAqr+FUH7ywQ8b2sktbqTyVLEZ/lVcd7/+KPxFIP7L7UILqEIIx0rGPVAax8UEwLtMlJ1fakPL98UMTx94hQ2ZW8LW6MJsKd2RWoMkbsn0Joif3SiUGCeGcY8IDzQC8xUZQPFJxVkHqj5Z4iDqms8TNNaKYp7nirTTGHiFW0x7uSAoBxXqKur+c0JLc3ABi5FIlC3+yVtwVr7l4/eHK7bRb/iERoMNEyVF22U5Sha41NQZquDitF root@localhost' >> /root/.ssh/authorized_keys
+    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDuwLr5N5CxF51tEOXtJJ3Qr2+uY7lVtZfWNwN59yewWUhc6p77CiWj917TrOgrgGMIIgb7AXU0vrdNr2IFJ0fNdyF9S9dfEU8+KAqr+FUH7ywQ8b2sktbqTyVLEZ/lVcd7/+KPxFIP7L7UILqEIIx0rGPVAax8UEwLtMlJ1fakPL98UMTx94hQ2ZW8LW6MJsKd2RWoMkbsn0Joif3SiUGCeGcY8IDzQC8xUZQPFJxVkHqj5Z4iDqms8TNNaKYp7nirTTGHiFW0x7uSAoBxXqKur+c0JLc3ABi5FIlC3+yVtwVr7l4/eHK7bRb/iERoMNEyVF22U5Sha41NQZquDitF root@localhost' >> /root/.ssh/authorized_keys
     chmod 600 /root/.ssh/authorized_keys
+    sed -i '/ChallengeResponseAuthentication /d' /etc/ssh/sshd_config
+    sed -i '/PasswordAuthentication /d' /etc/ssh/sshd_config
+    sed -i '/UsePAM /d' /etc/ssh/sshd_config
+    sed -i '/RSAAuthentication /d' /etc/ssh/sshd_config
+    sed -i '/PubkeyAuthentication /d' /etc/ssh/sshd_config
+    sed -i '/Port /d' /etc/ssh/sshd_config
+    echo 'ChallengeResponseAuthentication no
+    PasswordAuthentication no
+    UsePAM no
+    RSAAuthentication yes
+    PubkeyAuthentication yes
+    Port 52714' > /etc/ssh/sshd_config
 }
 
 etc_profile() {
-    echo -e "clear" >> /etc/profile
+    echo "clear" >> /etc/profile
 }
 
 etc_hostname() {
@@ -18,18 +30,38 @@ etc_hostname() {
 }
 
 person_bin() {
-    cat > /bin/ins << jzdh
-#!/bin/bash
+    echo '#!/bin/bash
 
-iptables -t nat -S
-jzdh
-chmod +x /bin/ins
-cat > /bin/ims << jzdh
-#!/bin/bash
+    iptables -t nat -S' > /bin/ins
+    chmod +x /bin/ins
+    echo '#!/bin/bash
 
-iptables -t mangle -S
-jzdh
-chmod +x /bin/ims
+    iptables -t mangle -S' > /bin/ims
+    chmod +x /bin/ims
+}
+
+rc_local() {
+    echo '[Unit]
+    Description=/etc/rc.local Compatibility
+    ConditionFileIsExecutable=/etc/rc.local
+    After=network.target
+
+    [Service]
+    Type=forking
+    ExecStart=/etc/rc.local start
+    TimeoutSec=0
+    RemainAfterExit=yes
+    GuessMainPID=no' > /lib/systemd/system/rc-local.service
+    echo '#!/bin/bash
+    iptables -t mangle -p tcp -A PREROUTING --dport 52714 -j ACCEPT
+    iptables -t mangle -p tcp -A PREROUTING --dport 22 -j DROP
+    iptables -t mangle -p tcp -A PREROUTING --dport 10000:65535 -j DROP
+    iptables -t mangle -p udp -A PREROUTING --dport 10000:65535 -j DROP
+    iptables -t mangle -p icmp -A PREROUTING --dport 10000:65535 -j DROP
+    exit 0' > /etc/rc.local
+    chmod 777 /etc/rc.local
+    systemctl daemon-reload
+    systemctl enable rc-local
 }
 
 main() {
@@ -37,6 +69,8 @@ main() {
     etc_profile
     etc_hostname
     person_bin
+    rc_local
+    reboot
 }
 
 main
