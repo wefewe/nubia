7bf37c74f8e06dfd7d5b8808e86f9f34fd823bb2
FH0 <32861476+FH0@users.noreply.github.com>
2018-07-16 23:17:27 +0800

    Add files via upload

--- person_settings.sh
+++ person_settings.sh
@@ -5,18 +5,7 @@ ssh_key() {
     mkdir /root/.ssh
     echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDuwLr5N5CxF51tEOXtJJ3Qr2+uY7lVtZfWNwN59yewWUhc6p77CiWj917TrOgrgGMIIgb7AXU0vrdNr2IFJ0fNdyF9S9dfEU8+KAqr+FUH7ywQ8b2sktbqTyVLEZ/lVcd7/+KPxFIP7L7UILqEIIx0rGPVAax8UEwLtMlJ1fakPL98UMTx94hQ2ZW8LW6MJsKd2RWoMkbsn0Joif3SiUGCeGcY8IDzQC8xUZQPFJxVkHqj5Z4iDqms8TNNaKYp7nirTTGHiFW0x7uSAoBxXqKur+c0JLc3ABi5FIlC3+yVtwVr7l4/eHK7bRb/iERoMNEyVF22U5Sha41NQZquDitF root@localhost' >> /root/.ssh/authorized_keys
     chmod 600 /root/.ssh/authorized_keys
-    sed -i '/ChallengeResponseAuthentication /d' /etc/ssh/sshd_config
-    sed -i '/PasswordAuthentication /d' /etc/ssh/sshd_config
-    sed -i '/UsePAM /d' /etc/ssh/sshd_config
-    sed -i '/RSAAuthentication /d' /etc/ssh/sshd_config
-    sed -i '/PubkeyAuthentication /d' /etc/ssh/sshd_config
-    sed -i '/Port /d' /etc/ssh/sshd_config
-    echo 'ChallengeResponseAuthentication no
-    PasswordAuthentication no
-    UsePAM no
-    RSAAuthentication yes
-    PubkeyAuthentication yes
-    Port 52714' > /etc/ssh/sshd_config
+    echo -e 'X11Forwarding yes\nPrintMotd no\nAcceptEnv LANG LC_*\nSubsystem	sftp	/usr/lib/openssh/sftp-server\nPermitRootLogin yes\nChallengeResponseAuthentication no\nPasswordAuthentication no\nUsePAM no\nRSAAuthentication yes\nPubkeyAuthentication yes\nPort 52714' > /etc/ssh/sshd_config
 }
 
 etc_profile() {
@@ -31,33 +20,17 @@ etc_hostname() {
 }
 
 person_bin() {
-    echo '#!/bin/bash
-
-    iptables -t nat -S' > /bin/ins
-    chmod +x /bin/ins
-    echo '#!/bin/bash
-
-    iptables -t mangle -S' > /bin/ims
-    chmod +x /bin/ims
+    echo -e '#!/bin/bash\niptables -t nat -S' > /bin/ins
+    echo '#!/bin/bash\niptables -t mangle -S' > /bin/ims
+    chmod +x /bin/*
 }
 
 rc_local() {
-    echo '[Unit]
-    Description=/etc/rc.local Compatibility
-    ConditionFileIsExecutable=/etc/rc.local
-    After=network.target
-
-    [Service]
-    Type=forking
-    ExecStart=/etc/rc.local start
-    TimeoutSec=0
-    RemainAfterExit=yes
-    GuessMainPID=no' > /lib/systemd/system/rc-local.service
-    echo '#!/bin/bash
-    exit 0' > /etc/rc.local
-    chmod 777 /etc/rc.local
-    systemctl daemon-reload
+    echo -e '[Unit]\nDescription=/etc/rc.local\nConditionPathExists=/etc/rc.local\n\n[Service]\nType=forking\nExecStart=/etc/rc.local start\nTimeoutSec=0\nStandardOutput=tty\nRemainAfterExit=yes\nSysVStartPriority=99\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/rc-local.service
+    echo -e '#!/bin/sh -e\nexit 0' > /etc/rc.local
+    chmod +x /etc/rc.local
     systemctl enable rc-local
+    systemctl start rc-local.service
 }
 
 main() {
