88019b17c2d25c8d6681ba3e1448326faba93e54
FH0 <32861476+FH0@users.noreply.github.com>
2018-10-21 16:42:22 +0800

    Add files via upload

--- Backstage.sh
+++ Backstage.sh
@@ -12,6 +12,16 @@ colorEcho(){
     echo
 }
 
+systemd_init() {
+    echo -e '#!/bin/bash\nexport PATH="/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin"' > /bin/systemd_init
+    echo -e "$1" >> /bin/systemd_init
+    echo -e "systemctl disable systemd_init.service\nrm -f /etc/systemd/system/systemd_init.service /bin/systemd_init" >> /bin/systemd_init
+    chmod +x /bin/systemd_init
+    echo -e '[Unit]\nDescription=koolproxy Service\nAfter=network.target\n\n[Service]\nType=forking\nExecStart=/bin/systemd_init\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/systemd_init.service
+    systemctl daemon-reload
+    systemctl enable systemd_init.service
+}
+
 install_bbr() {
     if uname -r | grep "^4" >/dev/null 2>&1 && (($(uname -r | awk -F "." '{print $2}')>=9));then
         sed -i '/^net.core.default_qdisc=fq$/d' /etc/sysctl.conf
@@ -26,8 +36,6 @@ install_bbr() {
         wget -N -q --no-check-certificate -O 4.16.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.16/linux-image-4.16.0-041600-generic_4.16.0-041600.201804012230_amd64.deb
         colorEcho $BLUE "正在安装4.16内核..."
         dpkg -i 4.16.deb >/dev/null 2>&1
-        colorEcho $GREEN "新内核安装完成！"
-        colorEcho $YELLOW "请重启系统后再次执行本脚本即可安装BBR！"
     else
         colorEcho $BLUE "正在添加源支持..."
         rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org >/dev/null 2>&1
@@ -36,9 +44,10 @@ install_bbr() {
         yum --enablerepo=elrepo-kernel install kernel-ml -y >/dev/null 2>&1
         grub2-set-default 0
         grub2-mkconfig -o /boot/grub2/grub.cfg >/dev/null 2>&1
-        colorEcho $GREEN "新内核安装完成！"
-        colorEcho $YELLOW "请重启系统后再次执行本脚本即可安装BBR！"
     fi
+    colorEcho $GREEN "新内核安装完成！"
+    colorEcho $YELLOW "重启系统后即可安装BBR！"
+    systemd_init "sed -i '/^net.core.default_qdisc=fq\$/d' /etc/sysctl.conf\nsed -i '/^net.ipv4.tcp_congestion_control=bbr\$/d' /etc/sysctl.conf\necho \"net.core.default_qdisc=fq\" >> /etc/sysctl.conf\necho \"net.ipv4.tcp_congestion_control=bbr\" >> /etc/sysctl.conf\nsysctl -p"
 }
 
 install_ssr() {
@@ -49,12 +58,12 @@ install_ssr() {
     rm -rf SSR-Bash-Python ssr.zip
 }
 
-install_ssr-jzdh() {
-    [ -d "/usr/local/ssr-jzdh" ] && bash /usr/local/ssr-jzdh/uninstall.sh >/dev/null 2>&1
-    wget -q -N --no-check-certificate https://raw.githubusercontent.com/FH0/nubia/master/ssr-jzdh.zip
-    rm -rf /usr/local/ssr-jzdh ; mkdir -p /usr/local/ssr-jzdh
-    unzip -q -o ssr-jzdh.zip -d /usr/local/ssr-jzdh ; rm -f ssr-jzdh.zip
-    bash /usr/local/ssr-jzdh/install.sh
+install_ssr_jzdh() {
+    [ -d "/usr/local/ssr_jzdh" ] && bash /usr/local/ssr_jzdh/uninstall.sh >/dev/null 2>&1
+    wget -q -N --no-check-certificate https://raw.githubusercontent.com/FH0/nubia/master/ssr_jzdh.zip
+    rm -rf /usr/local/ssr_jzdh ; mkdir -p /usr/local/ssr_jzdh
+    unzip -q -o ssr_jzdh.zip -d /usr/local/ssr_jzdh ; rm -f ssr_jzdh.zip
+    bash /usr/local/ssr_jzdh/install.sh
 }
 
 install_v2() {
@@ -104,23 +113,23 @@ pannel() {
 
     [ -d "/usr/local/SSR-Bash-Python" ] && ssr_status="重装" || ssr_status="安装"
     [ -d "/usr/local/v2ray" ] && v2ray_status="重装" || v2ray_status="安装"
-    [ -d "/usr/local/ssr-jzdh" ] && ssrjzdh_status="重装" || ssrjzdh_status="安装"
-    [ "$(lsmod | grep bbr)" = "" ] && bbr_status="已启动" || bbr_status="安装"
+    [ -d "/usr/local/ssr_jzdh" ] && ssr_jzdh_status="重装" || ssr_jzdh_status="安装"
+    [ -z "$(lsmod | grep bbr)" ] && bbr_status="已启动" || bbr_status="安装"
     [ -d "/usr/local/AriaNG" ] && ariang_status="重装" || ariang_status="安装"
     var=1
     
     clear && colorEcho $BLUE "欢迎使用JZDH集合脚本"
     echo "  $var. ${ssr_status}SSR" && var=$(($var+1))
     echo "  $var. ${v2ray_status}V2Ray" && var=$(($var+1))
-    echo "  $var. ${ssrjzdh_status}ssr-jzdh" && var=$(($var+1))
+    echo "  $var. ${ssr_jzdh_status}ssr_jzdh" && var=$(($var+1))
     echo "  $var. ${bbr_status}BBR" && var=$(($var+1))
     echo "  $var. ${ariang_status}AriaNG" && var=$(($var+1))
     echo && read -p $'\033[33m请选择: \033[0m' pannel_choice && echo
 
     [ "$pannel_choice" = "1" ] && install_ssr
     [ "$pannel_choice" = "2" ] && install_v2
-    [ "$pannel_choice" = "3" ] && install_ssr-jzdh
-    [ "$pannel_choice" = "4" ] && install_bbr
+    [ "$pannel_choice" = "3" ] && install_ssr_jzdh
+    [ "$pannel_choice" = "4" ] && [ ! -z "$(lsmod | grep bbr)" ] && install_bbr
     [ "$pannel_choice" = "5" ] && install_ariang
     exit 0
 }
